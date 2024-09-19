d0a0cc79c9c9d21e4b1aa3503b77ebdded3adf38
HoangQuan152000 <nguyenvanhoangquan15@gmail.com>
2024-09-19 17:47:52 +0700

    first cm

---
+++ Step1/tutorial.cpp
@@ -0,0 +1,12 @@
+﻿// 19092024.cpp : Defines the entry point for the application.
+//
+
+#include "tutorial.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
